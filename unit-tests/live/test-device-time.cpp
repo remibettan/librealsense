cb8df7b72fc1e3bdefaf4b6ad978bffcb26f8a2a
B <12195470+Sebtous@users.noreply.github.com>
2026-07-02 14:49:27 +0200

    Fixed unit test

--- unit-tests/live/test-device-time.cpp
+++ unit-tests/live/test-device-time.cpp
@@ -1,36 +1,74 @@
 // License: Apache 2.0. See LICENSE file in root directory.
 // Copyright(c) 2026 RealSense, Inc. All Rights Reserved.
 
-//#test:device D400*
+//#test:device ANY
 
 #include "live-common.h"
 
 #include <cmath>
-#include <librealsense2/h/rs_device.h>
-#include <librealsense2/rs_device.hpp>
+#include <memory>
+#include <librealsense2/rs.hpp>
+#include <librealsense2/rs.h>
 
 
-TEST_CASE("device hardware time is positive", "[live][device-time]")
+TEST_CASE("C++ device hardware time is positive",
+    "[live][device-time][cpp]")
 {
     auto dev = find_first_device_or_exit();
 
-    SECTION("C++ API")
-    {
-        double const device_time_ms = dev.get_device_time_ms();
+    double const device_time_ms = dev.get_device_time_ms();
 
-        REQUIRE(std::isfinite(device_time_ms));
-        REQUIRE(device_time_ms > 0.0);
-    }
+    REQUIRE(std::isfinite(device_time_ms));
+    REQUIRE(device_time_ms > 0.0);
+}
 
-    SECTION("C API")
-    {
-        rs2_error* error = nullptr;
 
-        double const device_time_ms =
-            rs2_get_device_time_ms(dev.get(), &error);
+TEST_CASE("C device hardware time is positive",
+    "[live][device-time][c]")
+{
+    rs2_error* error = nullptr;
 
-        REQUIRE(error == nullptr);
-        REQUIRE(std::isfinite(device_time_ms));
-        REQUIRE(device_time_ms > 0.0);
-    }
+    using context_ptr =
+        std::unique_ptr<rs2_context, decltype(&rs2_delete_context)>;
+
+    using device_list_ptr =
+        std::unique_ptr<rs2_device_list,
+        decltype(&rs2_delete_device_list)>;
+
+    using device_ptr =
+        std::unique_ptr<rs2_device, decltype(&rs2_delete_device)>;
+
+    context_ptr context(
+        rs2_create_context(RS2_API_VERSION, &error),
+        rs2_delete_context);
+
+    REQUIRE(error == nullptr);
+    REQUIRE(context != nullptr);
+
+    device_list_ptr devices(
+        rs2_query_devices(context.get(), &error),
+        rs2_delete_device_list);
+
+    REQUIRE(error == nullptr);
+    REQUIRE(devices != nullptr);
+
+    int const device_count =
+        rs2_get_device_count(devices.get(), &error);
+
+    REQUIRE(error == nullptr);
+    REQUIRE(device_count > 0);
+
+    device_ptr device(
+        rs2_create_device(devices.get(), 0, &error),
+        rs2_delete_device);
+
+    REQUIRE(error == nullptr);
+    REQUIRE(device != nullptr);
+
+    double const device_time_ms =
+        rs2_get_device_time_ms(device.get(), &error);
+
+    REQUIRE(error == nullptr);
+    REQUIRE(std::isfinite(device_time_ms));
+    REQUIRE(device_time_ms > 0.0);
 }
