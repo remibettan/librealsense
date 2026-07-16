6f554a25f67f16c1a6a56f5fc9a84464a34b2a20
Remi Bettan <remi.bettan@realsenseai.com>
2026-07-16 15:57:56 +0300

    unit-tests/options: extend gyro sensitivity coverage to D555, add levels test

--- unit-tests/live/options/pytest-pipeline-set-device.py
+++ unit-tests/live/options/pytest-pipeline-set-device.py
@@ -1,8 +1,6 @@
 # License: Apache 2.0. See LICENSE file in root directory.
 # Copyright(c) 2024 RealSense, Inc. All Rights Reserved.
 
-# LibCI doesn't have D435i so //test:device D435I// is disabled for now
-
 import pytest
 import platform
 import pyrealsense2 as rs
@@ -10,7 +8,8 @@ import logging
 log = logging.getLogger(__name__)
 
 pytestmark = [
-    pytest.mark.device("D455"),
+    pytest.mark.device_each("D455"),
+    pytest.mark.device_each("D555"),
     pytest.mark.skipif(platform.machine() == "aarch64", reason="D455 not available on CI Jetson"),
 ]
 
@@ -20,6 +19,8 @@ gyro_sensitivity_value = 4.0
 def test_pipeline_set_device(test_device):
     dev, ctx = test_device
     motion_sensor = dev.first_motion_sensor()
+    if not motion_sensor.supports(rs.option.gyro_sensitivity):
+        pytest.skip("Gyro Sensitivity option not supported on this device/FW")
     pipe = rs.pipeline(ctx)
     pipe.set_device(dev)
 
