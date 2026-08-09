711e7e55625a74027c39425116e86c09cf9b7e2f
Remi Bettan <remi.bettan@realsenseai.com>
2026-08-09 18:03:10 +0300

    v4l2: replace get_xu_range size assert with logged exception

--- src/linux/backend-v4l2.cpp
+++ src/linux/backend-v4l2.cpp
@@ -1924,7 +1924,14 @@ namespace librealsense
                                               << static_cast< int >( control ));
             }
 
-            assert(size<=len);
+            if( size > len )
+            {
+                LOG_ERROR( "get_xu_range: UVC_GET_LEN returned size " << size
+                           << " > requested len " << len << " on control " << static_cast< int >( control ) );
+                throw linux_backend_exception( rsutils::string::from()
+                    << "get_xu_range: unexpected size " << size << " > " << len
+                    << " on control " << static_cast< int >( control ) );
+            }
 
             std::vector<uint8_t> buf;
             auto buf_size = std::max((size_t)len,sizeof(__u32));
